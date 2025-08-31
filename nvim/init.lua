a68e2bf859d7c593eff37bb752243bf3bf49b540
Sarah Lament <4612427+sarahlament@users.noreply.github.com>
2025-08-31 02:22:13 -0500

    few changes, nothing noteworthy

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,11 +6,9 @@ require("catppuccin").setup({
 vim.cmd.colorscheme "catppuccin"
 
 vim.opt.autoindent=true
-vim.opt.expandtab=true
-vim.opt.shiftwidth=2
-vim.opt.tabstop=2
+vim.opt.shiftwidth=4
+vim.opt.tabstop=4
 
 vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
 vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
 vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'none' })
-vim.api.nvim_set_hl(0, 'Pmenu', { bg = 'none' })
